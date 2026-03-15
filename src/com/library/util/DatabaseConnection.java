1b992eb60385d23824daeed583829d476db0a650
Roland Garcia <roland.gviviani@gmail.com>
2026-03-15 14:43:04 +0900

    refactor: move database credentials to properties file for security

--- src/com/library/util/DatabaseConnection.java
+++ src/com/library/util/DatabaseConnection.java
@@ -1,19 +1,39 @@
 package com.library.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
     // TODO: Constantes com URL, USER, PASSWORD
-    private static final String url = "jdbc:postgresql://localhost:5432/library_db";
-    private static final String username = "rolandgarcia";
-    private static final  String password = "";
+
+    private static Properties loadProperties() {
+        Properties props = new Properties();
+
+        try(FileInputStream input = new FileInputStream("db.properties")){
+            props.load(input);
+
+        }catch (IOException e){
+            throw new RuntimeException("Error loading db.properties" + e.getMessage());
+        }
+        return props;
+    }
+
 
     // TODO: Método getConnection() que retorna Connection
 
     public static Connection getConnection() throws SQLException {
+        Properties props = loadProperties();
+        String url = props.getProperty("db.url");
+        String username = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
         return DriverManager.getConnection(url, username, password);
     }
-
 }
+
+
